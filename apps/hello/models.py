0d41f15bf0fcd711fcbe65a4c6eda27b3960d17e
Sergiy Ignatenko <grydinywka@gmail.com>
2016-04-24 14:06:30 +0300

    ticket:1 create Person model and instance of one

--- apps/hello/models.py
+++ apps/hello/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+#name, surname, date of birth, bio, contacts
+class Person(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    date_of_birth = models.DateField()
+    biography = models.TextField()
+    phone_numb = models.CharField(max_length=256)
+    email = models.EmailField(max_length=254)
 
-# Create your models here.
+    def __unicode__(self):
+        return u"%s %s" % (self.first_name, self.last_name)
